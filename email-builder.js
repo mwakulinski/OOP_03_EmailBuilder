6fe9bc5594713c0860c8ff493ff31ec42e7431a6
Michal Wakulinski <michal.wakulinski1@gmail.com>
2022-02-15 10:17:09 +0100

    second commit - update email-builder

--- email-builder.js
+++ email-builder.js
@@ -1,105 +1,74 @@
+const Email = require("./email");
+const Validator = require("./validator");
+
 class EmailBuilder {
   constructor() {
-    this._mail = {
-      from: "",
-      to: "",
-      title: "",
-      cc: [],
-      bcc: [],
-      html: "",
-    };
+    this.reset();
+  }
+
+  reset() {
+    this.email = new Email();
   }
 
   setFrom(address) {
-    //walidacja
-    this._mail.from = address;
+    Validator.throwIfNotString(address);
+    Validator.throwIfNotEmail(address);
+    this.email.from = address;
     return this;
   }
 
   setTo(address) {
-    this._mail.to = address;
+    Validator.throwIfNotString(address);
+    Validator.throwIfNotEmail(address);
+    this.email.to = address;
     return this;
   }
 
   setTitle(title) {
-    this._mail.title = title;
+    Validator.throwIfNotString(title);
+    this.email.title = title;
     return this;
   }
 
-  setCc(ccAddresses) {
-    this._mail.cc = [...ccAddresses];
+  setCc(...ccAddresses) {
+    Validator.throwIfNotOnlyStringsInArr([...ccAddresses]);
+    Validator.throwIfNotOnlyEmailsInArr([...ccAddresses]);
+    this.email.cc = [...ccAddresses];
     return this;
   }
 
-  setBcc(bccAddresses) {
-    this._mail.bcc = [...bccAddresses];
+  setBcc(...bccAddresses) {
+    Validator.throwIfNotOnlyStringsInArr([...bccAddresses]);
+    Validator.throwIfNotOnlyEmailsInArr([...bccAddresses]);
+    this.email.bcc = [...bccAddresses];
     return this;
   }
 
   setHtml(text) {
-    this._mail.html = text;
+    Validator.throwIfNotString(text);
+    this.email.html = text;
     return this;
   }
 
-  // Stwórz metody które będą zmieniać parametry from, to, title, cc, bcc, html
+  getEmail() {
+    console.log(this.email);
+  }
 
   buildMail = () => {
-    Verification.checkIfHasOnlyStringOrUndefined(Object.values(this._mail));
-    Verification.throwIfNoAdresseeOrReceipant(this._mail);
+    this.throwIfNoAdresseeOrReceipant();
+    const result = JSON.stringify(this.email);
     this.reset();
-    return JSON.stringify(this._mail);
+    return result;
     // return this._mail;
   };
-}
 
-class Verification {
-  //
-  static throwIfNoAdresseeOrReceipant = (obj) => {
-    if (obj.from.length === 0 || obj.to.length === 0) {
+  throwIfNoAdresseeOrReceipant = () => {
+    if (this.email.from.length === 0 || this.email.to.length === 0) {
       throw new Error(
-        "Before sending an email you mast enter Adressee and Receipant email"
+        "Before sending an email you must enter Adressee and Receipant email"
       );
     }
   };
-
-  static throwIfNotString = (arr) => {
-    if (!this.checkIfHasOnlyStringOrUndefined(arr)) {
-      throw new Error("All values must be string");
-    }
-  };
-
-  //przyjrzyj sie jeszcze raz
-  static checkIfHasOnlyStringOrUndefined = (arr) => {
-    return arr.every((item) => {
-      if (typeof item === "object") {
-        return this.checkIfHasOnlyStringOrUndefined(item);
-      }
-      return typeof item === "string" || item === undefined;
-    });
-  };
 }
 
-const newEmail = new EmailBuilder()
-  .setFrom("michal.michal.o2pl")
-  .setTo("mich.mich@op.pl")
-  .setTitle("Jak się masz")
-  .setCc("Basia.ww@op.pl", "Kasia.ww@op.pl")
-  .buildMail();
-
-console.log(newEmail);
-const newEmail1 = new EmailBuilder()
-  .setFrom("michal.michal.o2pl")
-  .setTo("mich.mich@op.pl")
-  .buildMail();
-
-console.log(newEmail1);
-
-const newEmail2 = new EmailBuilder()
-  .setFrom("michal.michal.o2pl")
-  .setTo("mich.mich@op.pl")
-  .setHtml(
-    "balaalajfsakdjhfsiludfhgiulsdf dsifhg ghdkjfg djhfg dsjfhg dfhgdkjf gdhfg dkjhfg dfgd fgjhdkfjg dhfg djfhg dkfg"
-  )
-  .buildMail();
-
-console.log(newEmail2);
+module.exports = EmailBuilder;
